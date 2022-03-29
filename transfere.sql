21a78cc43e956015fed540b7ff23c2419f5ae0d2
bruhenri12 <61971775+bruhenri12@users.noreply.github.com>
2022-03-29 20:43:28 -0300

    Atualização transfere.sql
    alterado CPF e chaves primárias

--- transfere.sql
+++ transfere.sql
@@ -3,12 +3,12 @@ data DATE,
 horario TIMESTAMP,
 numero_agencia VARCHAR(100),
 numero_conta VARCHAR(100), 
-cpf_auditor VARCHAR(100),
+cpf VARCHAR(100),
 
-CONSTRAINT Transfere_pkey PRIMARY KEY (data,horario),
+CONSTRAINT Transfere_pkey PRIMARY KEY (numero_agencia,numero_conta,cpf),
 
-CONSTRAINT Transfere_fkey_auditor FOREIGN KEY (cpf_auditor)
-REFERENCES Auditor(cpf_auditor),
+CONSTRAINT Transfere_fkey_auditor FOREIGN KEY (cpf)
+REFERENCES Auditor(cpf),
 
 CONSTRAINT Transfere_fkey_conta_orig FOREIGN KEY (numero_agencia, numero_conta)
 REFERENCES Conta(numero_agencia, numero_conta),
