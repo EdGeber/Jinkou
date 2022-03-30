7575d6b669668137f4767eaf37460dcf7c684bcd
bruhenri12 <61971775+bruhenri12@users.noreply.github.com>
2022-03-30 13:20:41 -0300

    atualizadas chaves primarias

--- transfere.sql
+++ transfere.sql
@@ -7,7 +7,7 @@ numero_agencia_dest VARCHAR(100),
 numero_conta_dest VARCHAR(100),
 cpf_auditor VARCHAR(100),
 
-CONSTRAINT Transfere_pkey PRIMARY KEY (data, horario),
+CONSTRAINT Transfere_pkey PRIMARY KEY (data, horario,numero_agencia_orig,numero_conta_orig,numero_agencia_dest,numero_conta_dest),
 
 CONSTRAINT Transfere_fkey_auditor FOREIGN KEY (cpf_auditor)
 REFERENCES Auditor(cpf),
