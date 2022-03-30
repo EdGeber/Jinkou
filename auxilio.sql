a2c4f85e5eb42f45168c8c75e6523535f5986880
Matheus Paixão <mpgs@cin.ufpe.br>
2022-03-30 12:20:52 -0300

    Adding CREATE SEQUENCE to the auxilio table

--- auxilio.sql
+++ auxilio.sql
@@ -1,11 +1,13 @@
-CREATE TABLE Auxilio(
-cod_auxilio varchar(5),
-nome_auxilio varchar(100) NOT NULL,
-CONSTRAINT Auxilio_pkey PRIMARY KEY(cod_auxilio));
+CREATE TABLE Auxilio( 
+cod_auxilio varchar(5), 
+nome_auxilio varchar(100) NOT NULL, 
+CONSTRAINT Auxilio_pkey PRIMARY KEY(cod_auxilio))
 
-INSERT INTO Auxilio VALUES (03541,'Moradia Estudantil');
-INSERT INTO Auxilio VALUES (02653,'Auxílio-Creche');
-INSERT INTO Auxilio VALUES (54513,'Bolsa Família');
-INSERT INTO Auxilio VALUES (45223,'Auxílio Emergencial');
-INSERT INTO Auxilio VALUES (77368,'Auxílio Internet');
-INSERT INTO Auxilio VALUES (00646,'Vale Transporte');
+CREATE SEQUENCE auxilio_seq INCREMENT BY 1 START WITH 1
+
+INSERT INTO Auxilio VALUES (auxilio_seq.nextval,'Moradia Estudantil');
+INSERT INTO Auxilio VALUES (auxilio_seq.nextval,'Auxílio-Creche');
+INSERT INTO Auxilio VALUES (auxilio_seq.nextval,'Bolsa Família');
+INSERT INTO Auxilio VALUES (auxilio_seq.nextval,'Auxílio Emergencial');
+INSERT INTO Auxilio VALUES (auxilio_seq.nextval,'Auxílio Internet');
+INSERT INTO Auxilio VALUES (auxilio_seq.nextval,'Vale Transporte');
