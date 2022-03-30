5efd8ae49ee7d5a9916f8d946da8cbf72f1a87ad
addaesg <casg@cin.ufpe.br>
2022-03-30 13:47:27 -0300

    organização eu acho

--- ocupacao.sql
+++
@@ -1,7 +0,0 @@
-create table Ocupacao(  
-  cpf_origem varchar(100),
-  ocupacao varchar(100),
-  
-  constraint pk_ocupacao primary key (cpf_origem, ocupacao),
-  constraint fk_ocupacaoCliente foreign key (cpf_origem) references Cliente(cpf)
-);
--- telefone.sql
+++
@@ -1,7 +0,0 @@
-create table Telefone(  
-  cpf_origem varchar(100),
-  telefone varchar(100),
-  
-  constraint pk_telefone primary key (cpf_origem, telefone),
-  constraint fk_telefonePessoa foreign key (cpf_origem) references Pessoa(cpf)
-);
