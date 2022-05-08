4554ecef9ca5f5a431c2f6ae81d79e19e9c5e0bd
Matheus Paixão <mpgs@cin.ufpe.br>
2022-05-08 16:56:24 -0300

    Adição de consultas

--- AV6/consultas_ev6.sql
+++ AV6/consultas_ev6.sql
@@ -9,3 +9,23 @@ SELECT O.* FROM tb_cliente c, TABLE(c.ocupacoes) O WHERE c.cpf = '001';
 -- CONSULTA A NESTED TABLE
 -- Retorna os dependentes da pessoa com cpf 462
 SELECT dep.* FROM TB_RELAC_DEPENDENTE_PESSOA rdp, TABLE(rdp.dependentes) dep WHERE rdp.cpf = '462';
+
+-- CONSULTA À TABELA tb_transfere
+SELECT data, horario, valor, status, motivo, t.conta_orig.numero_agencia AS numero_agencia_orig, t.conta_orig.numero_conta AS numero_conta_orig, t.conta_dest.numero_agencia AS numero_agencia_dest, t.conta_dest.numero_conta AS numero_conta_dest, DEREF(t.auditor).cpf AS cpf_auditor FROM tb_transfere t;
+
+-- BLOCO ANÔNIMO PARA PRINTAR AS INFORMAÇÕES DAS CONTAS DO CLIENTE QUE TEM O CPF 001 USANDO UM CURSOR
+DECLARE
+    CURSOR contas_cpf_movimenta IS SELECT DEREF(m.conta), DEREF(m.cliente) FROM tb_movimenta m WHERE m.cliente.cpf = '001';
+    cnt tp_conta;
+    cli tp_cliente;
+    BEGIN
+        OPEN contas_cpf_movimenta;
+        LOOP
+            FETCH contas_cpf_movimenta INTO cnt, cli;
+            EXIT WHEN contas_cpf_movimenta%NOTFOUND;
+            DBMS_OUTPUT.PUT_LINE('Proprietário da conta: '||cli.primeiro_nome||' '||cli.sobrenomes_centrais||' '||cli.ultimo_nome);
+            cnt.exibirDetalhesConta();
+            DBMS_OUTPUT.PUT_LINE(CHR(10));
+        END LOOP;
+        CLOSE contas_cpf_movimenta;
+    END;
